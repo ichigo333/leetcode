f77fbba849c375fb2de6825f0f0ed3196e152ac4
Alex <alex333sh@gmail.com>
2024-11-02 21:44:42 -0400

    796 rotate string official solutions

--- python/easy/796_rotate_string/solution_796.py
+++ python/easy/796_rotate_string/solution_796.py
@@ -1,8 +1,39 @@
 class Solution:
     def rotateString(self, s: str, goal: str) -> bool:
+        #if len(s) == 0 or len(goal) == 0: return False
+        if len(s) != len(goal): return False
+        
         for x in range(0, len(s)):
-            last = s[-1]
+            last = s[-1:]
             s = last + s[:-1]
             if s == goal: return True
         
         return False
+
+    #O(n2)
+    def officialSolution(self, s: str, goal: str) -> bool:
+        if len(s) != len(goal):
+            return False
+        length = len(s)
+
+        # Try all possible rotations of the string
+        for _ in range(length):
+            # Perform one rotation
+            s = s[1:] + s[0]
+            if s == goal:
+                return True
+        return False
+    
+    #O(n)
+    def officialConcatenationCheck(self, s: str, goal: str) -> bool:
+        # Check if the lengths are different
+        if len(s) != len(goal):
+            return False
+
+        # Create a new string by concatenating 's' with itself
+        doubled_string = s + s
+
+        # Use find to search for 'goal' in 'doubledString'
+        # If find returns an index that is not -1
+        # then 'goal' is a substring
+        return doubled_string.find(goal) != -1
--- python/easy/796_rotate_string/test_796.py
+++ python/easy/796_rotate_string/test_796.py
@@ -10,6 +10,18 @@ class Test796Solution(unittest.TestCase):
     def test_example2(self):
         solution = Solution()
         self.assertEqual(solution.rotateString("abcde","abced"), False)
+        
+    def test_rotateString_returnFalse_ifSandGoalAreDifferentLengths(self):
+        solution = Solution()
+        self.assertEqual(solution.rotateString("abcde","abceddef"), False)
+        
+    def test_official(self):
+        solution = Solution()
+        self.assertEqual(solution.officialSolution("abcde","cdeab"), True)
+        
+    def test_officialConcatenationCheck(self):
+        solution = Solution()
+        self.assertEqual(solution.officialConcatenationCheck("abcde","cdeab"), True)
          
 if __name__ == '__main__':
     unittest.main()
