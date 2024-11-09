5b8fdb85775ec77c6fc75e6eeddb22ad9bcf01d9
Alex <alex333sh@gmail.com>
2024-11-08 21:35:21 -0500

    20. Valid Parentheses

---
+++ csharp/SolutionTests/easy/Solution20Tests.cs
@@ -0,0 +1,53 @@
+﻿using NUnit.Framework;
+using Solutions;
+
+namespace SolutionTests.easy;
+
+[TestFixture]
+public class Solution20Tests {
+    private Solution20 _cut;
+
+    [SetUp]
+    public void SetUp() {
+        _cut = new Solution20();
+    }
+
+    [TestCase("()", true)]
+    [TestCase("()[]{}", true)]
+    [TestCase("([])", true)]
+    public void isValid_ReturnsTrue_ForEvenNumberOfBrackets(string input, bool expected) {
+        var actual = _cut.IsValid(input);
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void isValid_ReturnsFalse_IfOpeningBraceFollowedByDifferentClosingBrace() {
+        var actual = _cut.IsValid("(]");
+        Assert.That(actual, Is.EqualTo(false));
+    }
+
+    [Test]
+    public void isValid_ReturnsFalse_IfNumberOfBracesIsOdd() {
+        var actual = _cut.IsValid("(");
+        Assert.That(actual, Is.EqualTo(false));
+    }
+
+    [Test]
+    public void isValid_ReturnsFalse_IfTwoClosingBracesAtTheStart() {
+        var actual = _cut.IsValid("){");
+        Assert.That(actual, Is.EqualTo(false));
+    }
+
+
+    [Test]
+    public void isValid_ReturnsFalse_IfTwoClosingBraceWithoutOpening() {
+        var actual = _cut.IsValid("(){}}{");
+        Assert.That(actual, Is.EqualTo(false));
+    }
+
+    [Test]
+    public void isValid_ReturnsFalse_IfBracesAreOpenOnes() {
+        var actual = _cut.IsValid("({[");
+        Assert.That(actual, Is.EqualTo(false));
+    }
+}
---
+++ csharp/Solutions/easy/20_valid_parentheses/Solution20.cs
@@ -0,0 +1,46 @@
+﻿namespace Solutions;
+
+public class Solution20 {
+    // Accepted - beats 98% time 35% mem
+    public bool IsValid(string input) {
+        if (input.Length % 2 != 0) return false;
+
+        var stack = new Stack<char>();
+        foreach (char brace in input) {
+            if (brace == '(' || brace == '[' || brace == '{') { stack.Push(brace); }
+            else if (stack.Count > 0 &&
+                ((stack.Peek() == '(' && brace == ')') ||
+                (stack.Peek() == '{' && brace == '}') ||
+                (stack.Peek() == '[' && brace == ']'))) {
+                stack.Pop();
+            }
+            else
+                return false;
+            }
+        return stack.Count == 0;
+     }
+
+        
+
+
+    // Accepted - beats 30% time 29% mem
+    public bool IsValidFirstAttempt(string input) {
+        var braceDict = new Dictionary<char, char> {
+            { '(', ')' },
+            { '[', ']' },
+            { '{', '}' }
+        };
+
+        if (input.Length % 2 != 0) return false;
+        if (braceDict.ContainsValue(input[0])) return false;
+
+        var stack = new Stack<char>();
+        foreach (char brace in input) {
+            if (braceDict.ContainsKey(brace)) { stack.Push(brace); }
+            else if (stack.Count != 0 && braceDict[stack.Peek()] == brace) { stack.Pop(); }
+            else { return false; }
+        }
+
+        return stack.Count == 0;
+    }
+}
