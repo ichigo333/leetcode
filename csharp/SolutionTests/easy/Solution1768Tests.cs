d5175dea51772b405e6ef3948b83d792f7fceb42
Alex <alex333sh@gmail.com>
2024-11-14 22:19:27 -0500

    1768. Merge Strings Alternately

---
+++ csharp/SolutionTests/easy/Solution1768Tests.cs
@@ -0,0 +1,52 @@
+﻿using NUnit.Framework;
+using Solutions.easy._1768_merge_strings_alternately;
+
+namespace SolutionTests.easy; 
+
+[TestFixture]
+internal class Solution1768Tests {
+    private Solution1768 _cut;
+
+    [SetUp]
+    public void SetUp() {
+        _cut = new Solution1768();
+    }
+
+    [Test]
+    public void MergeAlternately_ShallReturnAlternatedMergeOfTwoStrings_WhenTwoStringsAreEqualLength() {
+        var word1 = "abc";
+        var word2 = "pqr";
+        var actual = _cut.MergeAlternately(word1, word2);
+        var expected = "apbqcr";
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void MergeAlternately_ShallReturnAlternatedMergeOfTwoStrings_WhenWord1IsShorter() {
+        var word1 = "ab";
+        var word2 = "pqrs";
+        var actual = _cut.MergeAlternately(word1, word2);
+        var expected = "apbqrs";
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void MergeAlternately_ShallReturnAlternatedMergeOfTwoStrings_WhenWord2IsShorter() {
+        var word1 = "abcd";
+        var word2 = "pq";
+        var actual = _cut.MergeAlternately(word1, word2);
+        var expected = "apbqcd";
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void MergeAlternately_ShallReturnAlternatedMergeOfTwoStrings_WhenBothWordsAreSingleLetter() {
+        var word1 = "a";
+        var word2 = "b";
+        var actual = _cut.MergeAlternately(word1, word2);
+        var expected = "ab";
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+
+
+}
---
+++ csharp/Solutions/easy/1768_merge_strings_alternately/Solution1768.cs
@@ -0,0 +1,72 @@
+﻿using System.Text;
+
+namespace Solutions.easy._1768_merge_strings_alternately; 
+
+public class Solution1768 {
+
+    //accepted - 53% time 99% memory
+    //O(n)
+    public string MergeAlternately(string word1, string word2) {
+        var result = new StringBuilder();
+        int maxLength = Math.Max(word1.Length, word2.Length);
+
+        for (var index = 0; index < maxLength; index++) {
+            if (index < word1.Length) {
+                result.Append(word1[index]);
+            }
+
+            if (index < word2.Length) {
+                result = result.Append(word2[index]);
+            }
+        }
+        return result.ToString();
+    }
+
+    //accepted - 46% time 94% memory
+    //with while loop
+    public string MergeAlternatelyAttempt2(string word1, string word2) {
+        var result = new StringBuilder();
+        var index = 0;
+
+        while (index < word1.Length || index < word2.Length) {
+            if (index < word1.Length) {
+                result.Append(word1[index]);
+            }
+
+            if (index < word2.Length) {
+                result = result.Append(word2[index]);
+            }
+            index++;
+        }
+        
+        return result.ToString();
+    }
+
+    //Solution from comments
+    //beats 80% time and 34% memory
+    public string MergeAlternatelySolution(string word1, string word2) {
+        string result = "";
+        int w1 = word1.Length;
+        int w2 = word2.Length;
+        int i = 0;
+        int j = 0;
+        while (i < w1 && j < w2) {
+            result += word1[i];
+            result += word2[j];
+            i++;
+            j++;
+        }
+
+        while (i < w1) {
+            result += word1[i];
+            i++;
+        }
+
+        while (j < w2) {
+            result += word2[j];
+            j++;
+        }
+
+        return result;
+    }
+}
