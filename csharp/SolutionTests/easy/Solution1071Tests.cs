dacd50903c05d47b64b0ae6da44dac2702ec1211
Alex <alex333sh@gmail.com>
2024-11-15 23:24:12 -0500

    1071. Greatest Common Divisor of Strings

---
+++ csharp/SolutionTests/easy/Solution1071Tests.cs
@@ -0,0 +1,59 @@
+﻿using NUnit.Framework;
+using Solutions.easy._1071_greatest_common_divisor_of_strings;
+
+namespace SolutionTests.easy;
+
+[TestFixture]
+internal class Solution1071Tests {
+    private Solution1071 _cut;
+
+    [SetUp]
+    public void SetUp() {
+        _cut  = new Solution1071();
+    }
+
+    [Test]
+    public void GcdOfStrings_ShallReturnStr2_WhenBothAreDivisibleByStr2() {
+        var string1 = "ABCABC";
+        var string2 = "ABC";
+        var actual = _cut.GcdOfStrings(string1, string2);
+        var expected = "ABC";
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void GcdOfStrings_ShallReturnAB_WhenStr1CannotBeFullyDividedByStr2() {
+        var string1 = "ABABAB";
+        var string2 = "ABAB";
+        var actual = _cut.GcdOfStrings(string1, string2);
+        var expected = "AB";
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void GcdOfStrings_ShallReturnEmptyString_WhenStringsAreNotDivisible() {
+        var string1 = "LEET";
+        var string2 = "CODE";
+        var actual = _cut.GcdOfStrings(string1, string2);
+        var expected = "";
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void GcdOfStrings_ShallReturnEmptyAB_WhenBothStringsAreAB() {
+        var string1 = "AB";
+        var string2 = "AB";
+        var actual = _cut.GcdOfStrings(string1, string2);
+        var expected = "AB";
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void GcdOfStrings_ShallReturnEmptyABC_WhenOnlyABCMatches() {
+        var string1 = "ABCDEF";
+        var string2 = "ABC";
+        var actual = _cut.GcdOfStrings(string1, string2);
+        var expected = "";
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+}
---
+++ csharp/Solutions/easy/1071_greatest_common_divisor_of_strings/Solution1071.cs
@@ -0,0 +1,56 @@
+﻿using System.Numerics;
+
+namespace Solutions.easy._1071_greatest_common_divisor_of_strings;
+
+public class Solution1071 {
+
+    //accepted - 100% time 38% memory
+    //O(m+n)?
+    //possibly O(min(m,n)⋅(m+n))
+    public string GcdOfStrings(string str1, string str2) {
+        if (str1 + str2 != str2 + str1) { return ""; }
+
+        var str1Length = str1.Length;
+        var str2Length = str2.Length;
+        var minLength = Math.Min(str1Length, str2Length);
+
+        for (var i = minLength; i > 0; i--) {
+            if ((str1Length % i == 0 && str2Length % i == 0)
+                && str1[..i] == str2[..i]) {
+                    return str1[..i];
+            }
+        }
+           
+        return "";
+    }
+
+
+    //Official solution - using a library for some god damn reason
+    //converted from C++ below
+    //O(m+n)
+    public string GcdOfStringsOfficial(string str1, string str2) {
+        // Check if they have non-zero GCD string.
+        if (str1 + str2 != str2 + str1) {
+            return "";
+        }
+
+        // Get the GCD of the two lengths.
+        var gcdLength = BigInteger.GreatestCommonDivisor(str1.Length, str2.Length);
+        return str1.Substring(0, (int)gcdLength);
+    }
+
+    //Official C++ version
+    //class Solution {
+    //    public:
+    //string gcdOfStrings(string str1, string str2) {
+    //        // Check if they have non-zero GCD string.
+    //        if (str1 + str2 != str2 + str1) {
+    //            return "";
+    //        }
+
+    //        // Get the GCD of the two lengths.
+    //        int gcdLength = gcd(str1.size(), str2.size());
+    //        return str1.substr(0, gcdLength);
+    //    }
+    //};
+}
