70b18c2a3674cae226b1ccca5b8652754decf030
Alex <alex333sh@gmail.com>
2024-11-19 22:37:36 -0500

    1431. Kids With the Greatest Number of Candies

---
+++ csharp/SolutionTests/easy/Solution1431Tests.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+using Solutions.easy._1431_kids_with_the_greatest_number_of_candies;
+
+namespace SolutionTests.easy;
+
+[TestFixture]
+internal class Solution1431Tests {
+    private Solution1431 _cut;
+
+    [SetUp]
+    public void SetUp() {
+        _cut = new Solution1431();
+    }
+
+    [Test]
+    public void KidsWithCandies_ShallReturnAllExceptIndexThree_WhenIndexThreeIsNotGreatest() {
+        bool[] expected = [true, true, true, false, true];
+        int[] candies = [2, 3, 5, 1, 3];
+        var actual = _cut.KidsWithCandies(candies, 3);
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void KidsWithCandies_ShallReturnTrueOnlyForIndexZero_WhenExtraCandiesIsOne() {
+        bool[] expected = [true, false, false, false, false];
+        int[] candies = [4, 2, 1, 1, 2];
+        var actual = _cut.KidsWithCandies(candies, 1);
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void KidsWithCandies_ShallReturnForTwoOfTheThree_WhenFirstAndLastIsGreatesAndEqual() {
+        bool[] expected = [true, false, true];
+        int[] candies = [12, 1, 12];
+        var actual = _cut.KidsWithCandies(candies, 10);
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+
+}
---
+++ csharp/Solutions/easy/1431_kids_with_the_greatest_number_of_candies/Solution1431.cs
@@ -0,0 +1,34 @@
+﻿namespace Solutions.easy._1431_kids_with_the_greatest_number_of_candies;
+
+public class Solution1431 {
+
+    //accepted - beats 60% time and 49% mem
+    //attempt1 - brute force
+    public IList<bool> KidsWithCandies(int[] candies, int extraCandies) {
+        bool[] result = new bool[candies.Length];
+
+        for (int i = 0; i < candies.Length; i++) {
+            var currentMax = candies[i] + extraCandies;
+            var isMax = true;
+            for (int j = 0; j < candies.Length; j++) {
+                if (candies[j] > currentMax) { isMax = false; break; }
+            }
+            result[i] = isMax;
+        }
+
+        return result;
+    }
+
+    //official - somehow worse?
+    //beats 56% time and 59% mem
+    public IList<bool> KidsWithCandiesOfficial(int[] candies, int extraCandies) {
+        bool[] result = new bool[candies.Length];
+        var maxCandies = candies.Max();
+
+        for (int i = 0; i < candies.Length; i++) {
+            result[i] = candies[i] + extraCandies >= maxCandies;
+        }
+            
+        return result;
+    }
+}
