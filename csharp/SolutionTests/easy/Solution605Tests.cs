de7a3ce6e6dc7ec9b7ee9160e992511517da27d0
Alex <alex333sh@gmail.com>
2024-11-21 22:46:30 -0500

    605. Can Place Flowers

---
+++ csharp/SolutionTests/easy/Solution605Tests.cs
@@ -0,0 +1,86 @@
+﻿
+using NUnit.Framework;
+using Solutions.easy._0605_can_place_flowers;
+
+
+namespace SolutionTests.easy;
+
+[TestFixture]
+internal class Solution605Tests {
+    private Solution605 _cut;
+
+    [SetUp]
+    public void SetUp() {
+        _cut = new Solution605();
+    }
+
+    [Test]
+    public void CanPlaceFlowers_Example1() {
+        int[] flowerbeds = [1, 0, 0, 0, 1];
+        var actual = _cut.CanPlaceFlowers(flowerbeds, 1);
+        Assert.That(actual, Is.EqualTo(true));
+    }
+
+    [Test]
+    public void CanPlaceFlowers_Example2() {
+        int[] flowerbeds = [1, 0, 0, 0, 1];
+        var actual = _cut.CanPlaceFlowers(flowerbeds, 2);
+        Assert.That(actual, Is.EqualTo(false));
+    }
+
+    [Test]
+    public void CanPlaceFlowers_ShallReturnTrue_IfFlowerCanBePlantedLast() {
+        int[] flowerbeds = [1, 0, 0];
+        var actual = _cut.CanPlaceFlowers(flowerbeds, 1);
+        Assert.That(actual, Is.EqualTo(true));
+    }
+
+    [Test]
+    public void CanPlaceFlowers_ShallReturnTrue_IfFlowerbedIsEmpty() {
+        int[] flowerbeds = [0, 0, 0];
+        var actual = _cut.CanPlaceFlowers(flowerbeds, 2);
+        Assert.That(actual, Is.EqualTo(true));
+    }
+
+    [Test]
+    public void CanPlaceFlowers_ShallReturnTrue_IfThereIsOneFlowerbedForOneFlower() {
+        int[] flowerbeds = [0];
+        var actual = _cut.CanPlaceFlowers(flowerbeds, 1);
+        Assert.That(actual, Is.EqualTo(true));
+    }
+
+    [Test]
+    public void CanPlaceFlowers_ShallReturnFalse_IfThereIsNoFlowerbedForOneFlower() {
+        int[] flowerbeds = [1];
+        var actual = _cut.CanPlaceFlowers(flowerbeds, 1);
+        Assert.That(actual, Is.EqualTo(false));
+    }
+
+    [Test]
+    public void CanPlaceFlowers_ShallReturnFalse_IfFlowerCannotBePlanted() {
+        int[] flowerbeds = [1, 0, 0, 1, 0];
+        var actual = _cut.CanPlaceFlowers(flowerbeds, 1);
+        Assert.That(actual, Is.EqualTo(false));
+    }
+
+    [Test]
+    public void CanPlaceFlowers_ShallReturnTrue_IfThereAreManyPlacesToPlant() {
+        int[] flowerbeds = [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+        var actual = _cut.CanPlaceFlowers(flowerbeds, 2);
+        Assert.That(actual, Is.EqualTo(true));
+    }
+
+    [Test]
+    public void CanPlaceFlowers_FailedOnSubmittion_ShouldActuallySetOneWhenPlanted() {
+        int[] flowerbeds = [1, 0, 0, 0, 0, 1];
+        var actual = _cut.CanPlaceFlowers(flowerbeds, 2);
+        Assert.That(actual, Is.EqualTo(false));
+    }
+
+    [Test]
+    public void CanPlaceFlowers_FailedOnSubmittion_ShallReturnTrue_IfNIsZero() {
+        int[] flowerbeds = [0, 0, 0, 0, 0, 1, 0, 0];
+        var actual = _cut.CanPlaceFlowers(flowerbeds, 0);
+        Assert.That(actual, Is.EqualTo(true));
+    }
+}
---
+++ csharp/Solutions/easy/0605_can_place_flowers/Solution605.cs
@@ -0,0 +1,56 @@
+﻿namespace Solutions.easy._0605_can_place_flowers;
+
+public class Solution605 {
+
+    //accepted - beats 99% time and 85% mem
+    //O(n)
+    public bool CanPlaceFlowers(int[] flowerbed, int n) {
+        if (n == 0) return true;
+        int flowersLeft = n;
+        int left = -1;
+        int right = -1;
+
+        for (int i = 0; i < flowerbed.Length; i++) {
+            left = i - 1 >= 0 ? flowerbed[i - 1] : flowerbed[i];
+            right = i + 1 < flowerbed.Length ? flowerbed[i + 1] : flowerbed[i];
+
+            if (left == 0 && flowerbed[i] == 0 && right == 0) {
+                flowerbed[i] = 1;
+                flowersLeft--;
+            }
+            if (flowersLeft == 0) return true;
+        }
+        return flowersLeft == 0;
+    }
+
+    //Official Optimized solution
+    //just like unoptimized but stops when all flowers are planted
+    //pretty much the same as above
+
+    //C++ implementation
+
+    //class Solution {
+    //    public:
+    //bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+    //        int count = 0;
+    //        for (int i = 0; i < flowerbed.size(); i++) {
+    //            // Check if the current plot is empty.
+    //            if (flowerbed[i] == 0) {
+    //                // Check if the left and right plots are empty.
+    //                bool emptyLeftPlot = (i == 0) || (flowerbed[i - 1] == 0);
+    //                bool emptyRightPlot = (i == flowerbed.size() - 1) || (flowerbed[i + 1] == 0);
+
+    //                // If both plots are empty, we can plant a flower here.
+    //                if (emptyLeftPlot && emptyRightPlot) {
+    //                    flowerbed[i] = 1;
+    //                    count++;
+    //                    if (count >= n) {
+    //                        return true;
+    //                    }
+    //                }
+    //            }
+    //        }
+    //        return count >= n;
+    //    }
+    //};
+}
